c8aebeb8880964f77b16da6e877da75fd5768969
yunemak <yeak558@gmail.com>
2026-07-09 12:05:18 +0300

    Add elements using js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
 
+const contentDiv = document.querySelector("#content");
+
+let welcomeH1 = document.createElement("h1");
+let sloganH2 = document.createElement("h2");
+let infoPara = document.createElement("p");
+
+welcomeH1.textContent = "Welcome Yunus's Restaurant";
+sloganH2.textContent = "High quality, healthy and the cheapest food here.";
+infoPara.textContent = " Our address: ... ";
+
+contentDiv.appendChild(welcomeH1);
+contentDiv.appendChild(sloganH2);
+contentDiv.appendChild(infoPara);
+
 console.log("Hello World!");
