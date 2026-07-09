cf99f2e5481d3fda15696901878c9ea65b8d229b
yunemak <yeak558@gmail.com>
2026-07-09 12:10:25 +0300

    Add home tab to event listener

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import "./styles.css";
 
 const contentDiv = document.querySelector("#content");
+const homeBtn = document.querySelector("#home-btn");
+const aboutBtn = document.querySelector("#about-btn");
+const menuBtn = document.querySelector("#menu-btn");
 
-let welcomeH1 = document.createElement("h1");
-let sloganH2 = document.createElement("h2");
-let infoPara = document.createElement("p");
+homeBtn.addEventListener("click", (e) => {
+	let welcomeH1 = document.createElement("h1");
+	let sloganH2 = document.createElement("h2");
+	let infoPara = document.createElement("p");
 
-welcomeH1.textContent = "Welcome Yunus's Restaurant";
-sloganH2.textContent = "High quality, healthy and the cheapest food here.";
-infoPara.textContent = " Our address: ... ";
+	welcomeH1.textContent = "Welcome Yunus's Restaurant";
+	sloganH2.textContent = "High quality, healthy and the cheapest food here.";
+	infoPara.textContent = " Our address: ... ";
 
-contentDiv.appendChild(welcomeH1);
-contentDiv.appendChild(sloganH2);
-contentDiv.appendChild(infoPara);
+	contentDiv.appendChild(welcomeH1);
+	contentDiv.appendChild(sloganH2);
+	contentDiv.appendChild(infoPara);
+})
 
-console.log("Hello World!");
+homeBtn.click();
