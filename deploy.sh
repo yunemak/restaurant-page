18a17e58a98aeee19034063c578d8eac86c94109
yunemak <yeak558@gmail.com>
2026-07-12 10:24:51 +0300

    Add deploy.sh script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git branch gh-pages || true
+git checkout gh-pages
+git merge main --no-edit
+
+npx webpack
+
+git add dist -f
+git commit -m "Deployment commit"
+
+git subtree push --prefix dist origin gh-pages
+
+git checkout main
